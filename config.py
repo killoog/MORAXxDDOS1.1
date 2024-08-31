df9afad866d0ddd4d55f1fc97de1974a8223b91f
killggon <nandbhandari0662@gmail.com>
2024-08-31 19:33:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY The_Fiend102
+#SCRIPT BY raiden_005
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("6319750205:AAElwPV4-nrC8VKbKma5OvzmD_a0HrUo3ZA")
+BOT_TOKEN=("7515901776:AAEI4kLpl1l1J-THGs6Bv7XTXRCvP4kgZVs")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("The_Fiend102")
+OWNER_USERNAME=("raiden_005")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("709227486")
+ADMIN_IDS=("1691757512")
 
 
 
